c161927d1cb5f69ec839b8c74afaf638463db9c1
James Bourbeau <jrbourbeau@users.noreply.github.com>
2024-03-01 15:26:57 -0600

    Run hourly with Prefect (#4)

--- preprocess.py
+++ preprocess.py
@@ -6,12 +6,17 @@ import os
 from collections import defaultdict
 
 import coiled
+import dask
+import dask_deltatable as ddt
 import deltalake
 import fsspec
 import pandas as pd
 import toolz
 from dask.distributed import LocalCluster, print, wait
+from prefect import flow
 from upath import UPath as Path
+
+dask.config.set({'dataframe.query-planning': True})
 
 logger = logging.getLogger(__name__)
 
@@ -21,12 +26,12 @@ if LOCAL:
     STORAGE_OPTIONS = {}
     Cluster = LocalCluster
 else:
-    OUTDIR = Path(f"s3://openscapes-scratch/etl-github")
+    OUTDIR = Path(f"s3://openscapes-scratch/etl-github-test")
     STORAGE_OPTIONS = {"AWS_REGION": "us-west-2", "AWS_S3_ALLOW_UNSAFE_RENAME": "true"}
     Cluster = functools.partial(
         coiled.Cluster,
         name="etl-github",
-        n_workers=50,
+        n_workers=1,
         region="us-west-2",
         shutdown_on_close=False,
     )
@@ -169,10 +174,25 @@ def write_delta(tables: dict[str, pd.DataFrame]):
             partition_by="date",
         )
 
+
+def parse_start_stop(start, stop):
+    """ If no provided, determine start and stop times """
+    if start is None:
+        t = deltalake.DeltaTable(
+            OUTDIR / "comment",
+            storage_options=STORAGE_OPTIONS,
+        )
+        actions = t.get_add_actions(flatten=True).to_pandas()
+        start = actions["max.created_at"].max().ceil("1h")
+    if stop is None:
+        # Current hour won't be fully populated yet, so subtract an hour
+        stop = pd.Timestamp.now().floor("1h") - datetime.timedelta(hours=1)
+    return start, stop
+
+
 def list_files(start, stop):
-    dates = pd.date_range(start, stop - datetime.timedelta(days=1), freq="d")
-    hours = [str(dt.date()) + f"-{h}.json.gz" for dt in dates for h in range(24)]
-    filenames = ["https://data.gharchive.org/" + hour for hour in hours]
+    dts = pd.date_range(start, stop, freq="1h")
+    filenames = [f"https://data.gharchive.org/{dt.date()}-{dt.hour}.json.gz" for dt in dts]
     return filenames
 
 
@@ -186,23 +206,75 @@ def compact(table):
     print(f"Compacted {table} table")
 
 
-if __name__ == "__main__":
-    filenames = list_files(
-        start=datetime.date(2024, 1, 1),
-        stop=datetime.date(2024, 1, 5),
-    )
+def query_data():
+    # These are not the actual queries we want.
+    # We're using these for now to get something going, but we
+    # should revisit them soon.
+    # (xref https://github.com/coiled/etl-github/pull/4#discussion_r1509525504)
+    watches = ddt.read_deltalake(OUTDIR / "watch")
+    repos = watches.repo.value_counts()
+    repos = repos[repos > 5].repartition(npartitions=1).to_frame()
 
+    commits = ddt.read_deltalake(OUTDIR / "commit")
+    commits = commits[~commits.username.str.contains("bot")]  # remove bots
+    major_commits = commits.merge(repos, on="repo")
+    df = major_commits[
+        major_commits.message.str.lower().str.contains(" dask")
+    ][["username", "repo", "message", "count"]]
+    df = df[~df.repo.str.startswith("dask/")]
+    out = df.sort_values("count", ascending=False)
+    outdir = OUTDIR / "dask" / "commits"
+    if outdir.exists():
+        outdir.fs.rm(str(outdir), recursive=True)
+        outdir.fs.makedirs(outdir)
+    ddt.to_deltalake(outdir, out, storage_options=STORAGE_OPTIONS)
+
+    # Same but for comments
+    comments = ddt.read_deltalake(OUTDIR / "comment")
+    major_comments = comments.merge(repos, on="repo")
+    df = major_comments[
+        major_comments.comment.str.lower().str.contains(" dask")
+    ][["username", "repo", "comment", "count"]]
+    df = df[~df.repo.str.startswith("dask/")]
+    out = df.sort_values("count", ascending=False)
+    outdir = OUTDIR / "dask" / "comments"
+    if outdir.exists():
+        outdir.fs.rm(str(outdir), recursive=True)
+        outdir.fs.makedirs(outdir)
+    ddt.to_deltalake(outdir, out, storage_options=STORAGE_OPTIONS)
+
+
+@flow(log_prints=True)
+def workflow(start=None, stop=None):
+    start, stop = parse_start_stop(start, stop)
+    filenames = list_files(start, stop)
+    print(f"{filenames = }")
     with Cluster() as cluster:
         if not LOCAL:
             cluster.send_private_envs({
                 "AWS_ACCESS_KEY_ID": os.environ["AWS_ACCESS_KEY_ID"],
                 "AWS_SECRET_ACCESS_KEY": os.environ["AWS_SECRET_ACCESS_KEY"],
             })
+            cluster.adapt(minimum=1, maximum=100)
         with cluster.get_client() as client:
-            client.restart()
             futures = client.map(process_file, filenames)
             futures = client.map(write_delta, futures, retries=10)
             wait(futures)
             tables = ["comment", "pr", "commit", "create", "watch", "fork"]
             futures = client.map(compact, tables)
             wait(futures)
+            query_data()
+
+
+if __name__ == "__main__":
+
+    # Initial bulk processing of historical data
+    workflow(
+        start=datetime.datetime(2024, 2, 29, hour=0),
+        stop=datetime.datetime(2024, 3, 1, hour=3),
+    )
+    # Process new data hourly
+    workflow.serve(
+        name="github-processing",
+        interval=datetime.timedelta(hours=1),
+    )
