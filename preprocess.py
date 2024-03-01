27b6ef3b7359c06e736bdf0f1c16e9e43e1bcf5a
James Bourbeau <jrbourbeau@gmail.com>
2024-03-01 11:33:08 -0600

    Run hourly with Prefect

--- preprocess.py
+++ preprocess.py
@@ -11,6 +11,7 @@ import fsspec
 import pandas as pd
 import toolz
 from dask.distributed import LocalCluster, print, wait
+from prefect import flow
 from upath import UPath as Path
 
 logger = logging.getLogger(__name__)
@@ -21,7 +22,7 @@ if LOCAL:
     STORAGE_OPTIONS = {}
     Cluster = LocalCluster
 else:
-    OUTDIR = Path(f"s3://openscapes-scratch/etl-github")
+    OUTDIR = Path(f"s3://openscapes-scratch/etl-github-test")
     STORAGE_OPTIONS = {"AWS_REGION": "us-west-2", "AWS_S3_ALLOW_UNSAFE_RENAME": "true"}
     Cluster = functools.partial(
         coiled.Cluster,
@@ -170,9 +171,8 @@ def write_delta(tables: dict[str, pd.DataFrame]):
         )
 
 def list_files(start, stop):
-    dates = pd.date_range(start, stop - datetime.timedelta(days=1), freq="d")
-    hours = [str(dt.date()) + f"-{h}.json.gz" for dt in dates for h in range(24)]
-    filenames = ["https://data.gharchive.org/" + hour for hour in hours]
+    dts = pd.date_range(start, stop, freq="1h")
+    filenames = [f"https://data.gharchive.org/{dt.date()}-{dt.hour}.json.gz" for dt in dts]
     return filenames
 
 
@@ -186,12 +186,26 @@ def compact(table):
     print(f"Compacted {table} table")
 
 
-if __name__ == "__main__":
-    filenames = list_files(
-        start=datetime.date(2024, 1, 1),
-        stop=datetime.date(2024, 1, 5),
-    )
+def parse_start_stop(start, stop):
+    """ If no provided, determine start and stop times """
+    if start is None:
+        t = deltalake.DeltaTable(
+            OUTDIR / "comment",
+            storage_options=STORAGE_OPTIONS,
+        )
+        actions = t.get_add_actions(flatten=True).to_pandas()
+        start = actions["max.created_at"].max().ceil("1h")
+    if stop is None:
+        # Current hour won't be fully populated yet, so subtract an hour
+        stop = pd.Timestamp.now().floor("1h") - datetime.timedelta(hours=1)
+    return start, stop
 
+
+@flow(log_prints=True)
+def workflow(start=None, stop=None):
+    start, stop = parse_start_stop(start, stop)
+    filenames = list_files(start, stop)
+    print(f"{filenames = }")
     with Cluster() as cluster:
         if not LOCAL:
             cluster.send_private_envs({
@@ -206,3 +220,17 @@ if __name__ == "__main__":
             tables = ["comment", "pr", "commit", "create", "watch", "fork"]
             futures = client.map(compact, tables)
             wait(futures)
+
+
+if __name__ == "__main__":
+
+    # Initial bulk processing of historical data
+    workflow(
+        start=datetime.datetime(2024, 2, 29, hour=0),
+        stop=datetime.datetime(2024, 3, 1, hour=3),
+    )
+    # Process new data hourly
+    workflow.serve(
+        name="github-processing",
+        interval=datetime.timedelta(hours=1),
+    )
